f7483f6c89565b471072ac2ba4baeb241a9f5c81
YAREN ŞAHİN <l5817021@std.yildiz.edu.tr>
2021-01-25 03:07:44 +0300

    Add project files.

---
+++ DictionaryExample/MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryExample
+{
+    public class MyDictionary<TStudentID, TStudentName>
+    {
+        TStudentID[] studentIDs;
+        TStudentName[] studentNames;
+
+        public MyDictionary()
+        {
+            studentIDs = new TStudentID[0];
+            studentNames = new TStudentName[0];
+
+        }
+
+        public void Add(TStudentID studentID,TStudentName studentName)
+        {
+            TStudentID[] tempID = studentIDs;
+            TStudentName[] tempNames = studentNames;
+
+            studentIDs = new TStudentID[studentIDs.Length + 1];
+            studentNames = new TStudentName[studentNames.Length + 1];
+
+            for (int i = 0; i < tempID.Length; i++)
+            {
+                studentIDs[i] = tempID[i];
+                studentNames[i] = tempNames[i];
+            }
+
+            studentIDs[studentIDs.Length - 1] = studentID;
+            studentNames[studentNames.Length - 1] = studentName;
+
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("-----------------List of Students-----------------");
+            for (int i = 0; i < studentIDs.Length; i++)
+            {
+                Console.WriteLine($"Id: {studentIDs[i]}  Name: {studentNames[i]}");
+            }
+
+        }
+    }
+}
---
+++ DictionaryExample/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DictionaryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(963, "Esra");
+            myDictionary.Add(112, "Faruk");
+            myDictionary.Add(587, "Fahriye");
+
+            myDictionary.Display();
+        }
+    }
+}
